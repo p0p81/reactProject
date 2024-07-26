3a1ee1faa05571cc22e8b9a93ab55a802e8c265a
p0p81 <153201905+p0p81@users.noreply.github.com>
2024-07-26 12:49:38 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,32 +4,28 @@ import Footer from "./components/footer/Footer";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
-import Catalog from "./components/catalog/Catalog"
+import Catalog from "./components/catalog/Catalog";
 import AddCard from "./components/addCard/AddCard";
 import Search from "./components/search/Search";
 
 function App() {
-
   return (
     <div id="box">
-        <Header/>
-          <main id="main-content">
-              <Routes>
-        
-                  <Route path="/" element={<Home/>} />
-                  <Route path="/login" element={<Login/>} />
-                  <Route path="/register" element={<Register/>} />
-                  <Route path="/catalog" element={<Catalog/>} />
-                  <Route path="/addCard" element={<AddCard/>} />
-                  <Route path="/search" element={<Search/>} />
+      <Header />
+      <main id="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/addCard" element={<AddCard />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </main>
 
-              </Routes>
-
-          </main>
-
-        <Footer/>
+      <Footer />
     </div>
-    )
+  );
 }
 
-export default App
+export default App;
